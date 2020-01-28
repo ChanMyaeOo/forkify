7e5fba7038338798d951a9f5f43ccffb930e46ea
chanmyaeoo <chanmyaeoo1996mgk@gmail.com>
2020-01-28 20:34:21 +0630

    added search action to the search form

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,33 @@
 import Search from './models/Search';
 
-const search = new Search('pizza');
-search.getRecipe();
-console.log(search);
+/* Global state of the app */
+/**
+ * - Search object
+ * - Current recipe
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {};
+
+const controlSearch = async () => {
+  // 1) Get query for search
+  const query = 'pizza';
+
+  if (query) {
+    // 2) New search object and add to the state
+    state.search = new Search(query);
+
+    // 3) Prepare to show in UI
+
+    // 4) Search for recipe
+    await state.search.getRecipe();
+
+    // 5) Render result on UI
+    console.log(state.search.result);
+  }
+};
+
+document.querySelector('.search').addEventListener('submit', e => {
+  e.preventDefault();
+  controlSearch();
+});
